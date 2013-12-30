c675c02ee63c73d8f8d7308327c8af0f6bea963c
gaorongchao <rongchaogao@gmail.com>
2013-12-30 09:10:34 +0800

    something about Object_oriented

---
+++ Object_Oriented/OO.pl
@@ -0,0 +1,39 @@
+sub word
+{
+	my $word=shift;
+	my $length=length($word);
+	my $first=substr($word,1,1);
+	print "$length $first\n";
+}
+
+word("how")
+
+=cut
+#package Dog; 
+
+#sub new {
+   my $pkg = shift;
+   my $name = shift;
+   return bless {name => $name}, $pkg;
+	#以后通过   $x->shout 这样的调用， 就会转化成 Dog::shout($x); 这样的调用。
+	#所以perl面向对象的本质就是使用bless，把数据（标量）跟包绑定起来。
+   # 一个hash，经过bless以后，就和一个包绑定了
+} 
+
+sub shout {
+   my $self = shift;
+   print "my name is " . $self->{name};
+}
+1;
+
+#这个就是基本的类定义。
+
+#调用是这样： my $x = Dog->new;    $x->shout();
+
+my $x=Dog->shout("Babby");
+print "$x\n";
+
+#键是理解箭头符号补插参数的作用。
+#$x->shout(...);  等于   Dog::shout($x, ...);   记住这个！！
+
+=cut
---
+++ Object_Oriented/Word.pm
@@ -0,0 +1,11 @@
+sub len
+{
+	print "length\n";
+}
+=cut
+sub len
+{
+	my $word=shift;
+	my $length=length($word);
+	print "$length\n";
+}
