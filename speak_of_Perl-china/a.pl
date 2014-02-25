a2f53a94c4ac685b35f7849df0c5da6db50ee897
gaorongchao <rongchaogao@gmail.com>
2014-02-25 08:46:21 +0800

    add sort the final result

--- speak_of_Perl-china/a.pl
+++ speak_of_Perl-china/a.pl
@@ -3,7 +3,7 @@ use warnings;
 use 5.16.3;
 use utf8;
 
-my (%hash,);
+my (%hash,%hash1,);
 my	$out_out = "result1.txt";
 open  my $out, '>', $out_out or die  "Fail open $out_out\n";
 
@@ -27,6 +27,15 @@ close  $in;
 
 foreach my $key1 (sort keys %hash)
 {
-	print $out "$key1\t$hash{$key1}[1]\t$hash{$key1}[0]\n";
+	#print $out "$key1\t$hash{$key1}[1]\t$hash{$key1}[0]\n";
+	$hash1{$hash{$key1}[0]}{$key1}=$hash{$key1}[1];
+}
+
+foreach my $key1 (sort {$b<=>$a} keys %hash1)
+{
+	foreach my $key2 (sort keys %{$hash1{$key1}})
+	{
+		print $out "$key2\t$hash1{$key1}{$key2}\t$key1\n";
+	}
 }
 close  $out;
