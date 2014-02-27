678f576a880814815146520f27bdc6da3e956905
gaorongchao <rongchaogao@gmail.com>
2014-02-27 17:04:00 +0800

    add the old way

--- data_for_path.pl
+++ data_for_path.pl
@@ -6,11 +6,11 @@ use Data::Dumper;
 sub data_for_path
 {
 	my $path = shift;
-	if (-f $path or -l $path)
+	if (-f $path or -l $path) # -f 判断file -l 判断link
 	{
 		return undef;
 	}
-	if (-d $path)
+	if (-d $path) # -d 判断文件夹
 	{
 		my %directory;
 		opendir PATH,$path or die "Cannot opendir $path:$!";
@@ -18,7 +18,7 @@ sub data_for_path
 		closedir PATH;
 		for my $name (@names)
 		{
-			next if $name eq '.' or $name eq '..';
+			next if $name eq '.' or $name eq '..'; # 排除. 和.. 文件
 			$directory{$name} = data_for_path("$path/$name");
 		}
 		return \%directory;
@@ -28,4 +28,44 @@ sub data_for_path
 }
 
 # 只需要给定一个目录名称
+# 结果是一个引用
 print Dumper(data_for_path("BioPerl"));
+
+my %data;
+sub dump_data_for_path
+{
+	my $path = shift;
+	my $data = shift;
+
+	if (not defined $data)
+	{
+		print "$path\n";
+		return;
+	}
+	foreach (sort keys %{$data})
+	{
+		dump_data_for_path("$path/$_",$$data{$_}); # 这个地方是$$
+	}
+}
+
+dump_data_for_path("BioPerl",data_for_path("BioPerl"));
+
+=cut
+# another way
+sub dump_data_for_path
+{
+	my $path = shift;
+	my $data = shift;
+
+	if (not defined $data)
+	{
+		print "$path\n";
+		return;
+	}
+	my %directory = %$data;
+
+	foreach (sort keys %directory)
+	{
+		dump_data_for_path("$path/$_",$directory($_)); # 这个地方是$$
+	}
+}
