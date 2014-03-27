05e18ddc2846fe6fd41974c07e23c010768f565e
gaorongchao <rongchaogao@gmail.com>
2014-03-27 23:09:43 +0800

    repair some question

--- Bio/Mega/Perl_to_use_Megacc.pl
+++ Bio/Mega/Perl_to_use_Megacc.pl
@@ -3,9 +3,13 @@ use warnings;
 use utf8;
 use 5.16.3;
 
-
+my	$out_out = "file.txt";
+open  my $out, '>', $out_out or die  "Fail open $out_out\n";
 my @files = glob "*.fasta";
 foreach my $file (@files)
 {
-	exec("M6CC.exe -a huashu.mao -d $file -o $file.out");
+	#print $out "D:\\Less_less_region\\$file\n";
+	#exec("M6CC.exe -a huashu.mao -d $file -o $file.out");
+	system ("M6CC.exe -a huashu.mao -d $file -o $file.out");
 }
+close  $out;
